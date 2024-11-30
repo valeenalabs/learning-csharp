74fbdc78b392977976109f1367b77a5713cb2e61
Valeena <64002698+valeenalabs@users.noreply.github.com>
2024-11-30 09:35:00 -0500

    Beginning of the FileService.cs

---
+++ NeuronNotes/Services/FileService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using NeuronNotes.Models;
+using NeuronNotes.Services.Interface;
+
+namespace NeuronNotes.Services;
+
+public class FileService : IFileService
+{
+    private readonly string _basePath = Path.Combine(
+        Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+        "NeuronNotes");
+
+    public FileService()
+    {
+        Directory.CreateDirectory(_basePath);
+    }
+
+    public async Task SaveNoteAsync(Note note)
+    {
+        var filePath = GetNoteFilePath(note.Id);
+        var json = JsonSerializer.Serialize(note);
+        await File.WriteAllTextAsync(filePath, json);
+    }
+
+    public async Task<Note?> LoadNoteAsync(Guid id)
+    {
+        var filePath = GetNoteFilePath(id);
+        if (!File.Exists(filePath)) return null;
+        
+        var json = await File.ReadAllTextAsync(filePath);
+        return JsonSerializer.Deserialize<Note>(json);
+    }
+
+    /// <inheritdoc />
+    public Task SaveAllNotesAsync(IEnumerable<Note> notes)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public Task<IEnumerable<Note>> LoadAllNotesAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    private string GetNoteFilePath(Guid id) =>
+        Path.Combine(_basePath, $"{id}.json");
+    
+}
---
+++ NeuronNotes/Services/Interface/IFileService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NeuronNotes.Models;
+
+namespace NeuronNotes.Services.Interface;
+
+public interface IFileService
+{
+    Task SaveNoteAsync (Note note);
+    Task<Note?> LoadNoteAsync (Guid id);
+    Task SaveAllNotesAsync (IEnumerable<Note> notes);
+    Task<IEnumerable<Note>> LoadAllNotesAsync ();
+}
